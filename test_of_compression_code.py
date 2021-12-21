3eed26b78df7270b56c31221a65537ed9a2b0578
Mohamed Ragab Hussein <85412361+Mohamed123098@users.noreply.github.com>
2021-12-21 12:28:00 -0800

    Add files via upload

---
+++ test_of_compression_code.py
@@ -0,0 +1,15 @@
+from compression_code import compression
+import sys
+with open('sample.xml') as file:
+    lines = file.read()
+text_file = open("Output.txt", "w")
+text_file.write(lines)
+text_file.close()
+path_of_File = "Output.txt"
+
+h = compression(path_of_File)
+
+output_path = h.compress_file()
+print("Compressed file path: " + output_path)
+
+
